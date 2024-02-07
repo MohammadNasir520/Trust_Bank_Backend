31a6796bf51e17853de3c9f483cdcb8d79586581
MohammadNasir520 <nasirahsan520@gmail.com>
2024-02-07 18:46:05 +0600

    user correction

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -4,6 +4,7 @@ import { UserController } from './user.controller';
 
 const router = express.Router();
 
+router.get('/', auth(), UserController.getAllFromDB);
 router.get('/:id', auth(), UserController.getByIdFromDB);
 router.patch('/:id', auth(), UserController.updateIntoDB);
 router.delete('/:id', auth(), UserController.deleteFromDB);
