6e1a353219575475b03738ef877859cb0d1a5e2a
MohammadNasir520 <nasirahsan520@gmail.com>
2024-02-07 18:36:15 +0600

    auth correction

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,30 +1,11 @@
 import express from 'express';
-import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import { UserController } from './user.controller';
 
 const router = express.Router();
 
-router.get(
-  '/',
-  // auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-  UserController.getAllFromDB
-);
-
-router.get(
-  '/:id',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-  UserController.getByIdFromDB
-);
-router.patch(
-  '/:id',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-  UserController.updateIntoDB
-);
-router.delete(
-  '/:id',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-  UserController.deleteFromDB
-);
+router.get('/:id', auth(), UserController.getByIdFromDB);
+router.patch('/:id', auth(), UserController.updateIntoDB);
+router.delete('/:id', auth(), UserController.deleteFromDB);
 
 export const UserRoutes = router;
